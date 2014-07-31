4ff37e64ea9dbb463373e2cc7f3beaecf9f06463
Vasil Vanchuk <v.vanchuk@tut.by>
2014-07-31 20:02:09 +0300

    Add first controller

---
+++ inc/controllers/main.php
@@ -0,0 +1,11 @@
+<?php
+
+class MainController
+{
+
+    public function __construct()
+    {
+        echo __CLASS__;
+    }
+
+}
--- index.php
+++ index.php
@@ -1,1 +1,11 @@
 <?php
+
+define('DEFAULT_CONTROLLER','main');
+
+$url = isset($_GET['url'])? trim($_GET['url']): DEFAULT_CONTROLLER;
+
+$controllerFileName = 'inc/controllers/'.$url.'.php';
+$controllerName = $url.'Controller';
+
+require_once($controllerFileName);
+$controller = new $controllerName;
