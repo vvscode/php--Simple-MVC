59aeaf67f62d7ab9b729f0ca30982aa84613cba4
Vasil Vanchuk <v.vanchuk@tut.by>
2014-07-31 20:28:18 +0300

    Move start logic to App class

---
+++ inc/app.php
@@ -0,0 +1,28 @@
+<?php
+
+class App
+{
+    public function __construct()
+    {
+        $url = isset($_GET['url'])? trim($_GET['url']): DEFAULT_CONTROLLER;
+        $urlParts = explode('/', $url);
+
+        $controllerFileName = 'inc/controllers/'.$urlParts[0].'.php';
+        $controllerName = $urlParts[0].'Controller';
+
+        if(file_exists($controllerFileName)){
+            require_once($controllerFileName);
+        } else {
+            throw new Exception('Controller not found');
+        }
+
+        $controller = new $controllerName;
+
+        if(isset($urlParts[1])){
+            $actionName = $urlParts[1].'Action';
+            if(method_exists($controller, $actionName)){
+                $controller->$actionName();
+            }
+        }
+    }
+} 
--- index.php
+++ index.php
@@ -2,17 +2,6 @@
 // Контроллер по-умолчанию
 define('DEFAULT_CONTROLLER','main');
 
-$url = isset($_GET['url'])? trim($_GET['url']): DEFAULT_CONTROLLER;
-$urlParts = explode('/', $url);
+require_once('inc/app.php');
 
-$controllerFileName = 'inc/controllers/'.$urlParts[0].'.php';
-$controllerName = $urlParts[0].'Controller';
-
-
-require_once($controllerFileName);
-$controller = new $controllerName;
-
-if(isset($urlParts[1])){
-    $actionName = $urlParts[1].'Action';
-    $controller->$actionName();
-}
+new App();
