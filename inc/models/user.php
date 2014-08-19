338715d372b5dba727820b0ca9a562fd578d0068
Vasil Vanchuk <v.vanchuk@tut.by>
2014-08-19 16:44:34 +0300

    Clear UserModel - remove array with users list

--- inc/models/user.php
+++ inc/models/user.php
@@ -2,15 +2,6 @@
 
 class UserModel extends Model
 {
-
-    protected static $usersList = array(
-        array('id' => 1, 'login' => 'admin', 'authkey' => 'admin', 'name' => 'Главный администратор', 'isAdmin' => TRUE),
-        array('id' => 2, 'login' => 'demo', 'authkey' => 'demo', 'name' => 'Демо пользователь', 'isAdmin' => FALSE),
-        array('id' => 3, 'login' => 'user', 'authkey' => 'user', 'name' => 'Пользователь, просто пользователь', 'isAdmin' => FALSE),
-        array('id' => 4, 'login' => 'test', 'authkey' => 'test', 'name' => 'Тестовый пользователь', 'isAdmin' => FALSE),
-        array('id' => 5, 'login' => 'root', 'authkey' => 'R00t', 'name' => 'Самый самый главный', 'isAdmin' => FALSE),
-    );
-
     public function __construct(array $properties = array())
     {
         parent::__construct();
