baaa4ea0f94cb146b82f97983b89955dc2f149d4
Vasil Vanchuk <v.vanchuk@tut.by>
2014-08-19 17:31:57 +0300

    Reformat code

--- test/UserModelTest.php
+++ test/UserModelTest.php
@@ -4,9 +4,9 @@ namespace test;
 require_once __DIR__.'/../inc/inc.php';
 
 class UserModelTest extends \PHPUnit_Framework_TestCase {
-//    public function testFailExample(){
-//        $this->assertTrue(FALSE, 'Example for failing test case');
-//    }
+    public function testFailExample(){
+        $this->assertTrue(FALSE, 'Example for failing test case');
+    }
 
     public function testSuccessExample(){
         $this->assertTrue(TRUE, 'Example for successed test case');
@@ -20,7 +20,9 @@ class UserModelTest extends \PHPUnit_Framework_TestCase {
 
         $this->assertTrue(method_exists('\UserModel', 'findBy'), 'UserModel should have findBy method');
         $user1 = \UserModel::findBy(array('id' => 1));
+
         $this->assertInstanceOf('\UserModel', $user1, 'If user exists - findBy should return UserModel');
+
         $userNotExists = \UserModel::findBy(array('id' => null));
         $this->assertNull($userNotExists, 'If user not exists - findBy should return null');
     }
